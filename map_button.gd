600fe9dcd35d652c2ec1272baf1762cd155ef3f8
SeppyDev <53712490+Seppy-Dev@users.noreply.github.com>
2025-03-16 00:54:05 +0000

    Disabled button scaling as it's currently broken

--- map_button.gd
+++ map_button.gd
@@ -1,26 +1,28 @@
-extends Node
+extends Button
 @export var location: PackedScene
-var default_scale: Vector2
+#var default_scale: Vector2
 
 
 func _ready() -> void:
-	default_scale = self.scale
+	#default_scale = self.scale
 	connect("pressed", Callable(self, "_on_pressed"))
-	connect("mouse_entered", Callable(self, "_on_mouse_entered"))
-	connect("mouse_exited", Callable(self, "on_mouse_exited"))
-	connect("button_down", Callable(self, "on_button_down"))
-	
+	#connect("mouse_entered", Callable(self, "_on_mouse_entered"))
+	#connect("mouse_exited", Callable(self, "on_mouse_exited"))
+	#connect("button_down", Callable(self, "on_button_down"))
+
+# Send player to selected location
 func _on_pressed() -> void:
 	if location:
 		get_tree().change_scene_to_packed(location)
 
-
 # Button Scaling
-func _on_button_down() -> void:
-	self.scale = default_scale * 0.8
-	
-func _on_mouse_entered() -> void:
-	self.scale = default_scale * 1.2
-	
-func _on_mouse_exited() -> void:
-	self.scale = default_scale
+#func _on_mouse_entered() -> void:
+	#self.scale = default_scale * 1.2
+	#
+#func _on_mouse_exited() -> void:
+	#self.scale = default_scale
+	#
+#func _on_button_down() -> void:
+	#self.scale = default_scale * 0.8
+
+#TODO: Fix the fucking scaling.
